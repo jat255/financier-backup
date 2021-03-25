a0afe0bd94c6d769619ba8e67b48b2e1e47fad8e
Joshua Taillon <jat255@gmail.com>
2021-03-24 23:25:51 -0600

    Move downloaded backup to folder of your choice

--- run_backup.sh
+++ run_backup.sh
@@ -3,5 +3,12 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 cd "${DIR}"
-
+source .env
 node index.js
+
+if [ -z ${DESTINATION+x} ]; then 
+  echo "Not moving any files because DESTINATION was not set"
+else 
+  echo "Moving backup file to ${DESTINATION}"
+  mv *backup.json "${DESTINATION}"
+fi
